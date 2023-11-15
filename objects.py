544ad0a40acf9b666792e06d15868e2f6057a3e0
Miguel <ivanmigueldoller@gmail.com>
2023-11-15 09:20:22 +0800

    connect object variables

--- objects.py
+++ objects.py
@@ -1,13 +1,14 @@
 class Employee:
 
-    def __init__(self,EmpID,EmpLName,EmpFName,Age,Address,DateofEmplymnt,JobID):
+    def __init__(self,EmpID,EmpLName,EmpFName,Age,Address,JobDescription,TypeOfEmplymnt,DateofEmplymnt):
         EmpID = None
         EmpLName = None
         EmpFName = None
         Age = None
         Address = None
+        TypeOfEmplymnt = None
         DateofEmplymnt = None
-        JobID = None
+        JobDescription = None
 
 
 
